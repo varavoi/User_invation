6c68aeda6f23a53e43892a23534fa4f5cd619ee1
varavoi <institut93@gmail,com>
2024-10-03 18:42:00 +0300

    В компоненте Users, передал все свойства объектов(массива данных полученных из сервера) в тело компонента User, и все данные о наших пользователей отображаются в соответствии с данными полученными из сервера

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -21,7 +21,7 @@ export const Users = ({ items, isLoading }) => {
         <ul className="users-list">
           {
           items.map((obj)=>(
-            <User/>
+            <User {...obj}/>
           ))
           }
         </ul>
