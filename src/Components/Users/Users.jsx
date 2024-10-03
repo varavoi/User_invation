69556c34899c3b5b2a36c826c7f2e81f3c3aaae4
varavoi <institut93@gmail,com>
2024-10-03 19:13:12 +0300

    Передал свойства searchValue и onChangeSearchValue в тело поискового поля input внутри компонента Users

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Skeleton } from './Skeleton';
 import { User } from './User';
 
-export const Users = ({ items, isLoading }) => {
+export const Users = ({ items, isLoading, searchValue, onChangeSearchValue }) => {
   return (
     <>
       <div className="search">
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input type="text" placeholder="Найти пользователя..." />
+        <input 
+          value = {searchValue}
+          onChange={onChangeSearchValue}
+          type="text" 
+          placeholder="Найти пользователя..." 
+        />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
