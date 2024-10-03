ddddd7d84ec6be6424e31b1b3510cbd076c99e5b
varavoi <institut93@gmail,com>
2024-10-03 18:36:34 +0300

    Написал логику, что каждый компонент User, получал данные о пользователя, и заполнялся динамически

--- src/Components/Users/User.jsx
+++ src/Components/Users/User.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export const User = () => (
+export const User = ({id, email, first_name, last_name, avatar}) => (
   <li>
     <div>
-      <img className="avatar" src="https://reqres.in/img/faces/1-image.jpg" alt="User" />
+      <img className="avatar" src={avatar} alt="User" />
       <div>
-        <h3>Amon Bower</h3>
+        <h3>{first_name} {last_name}</h3>
         <p>
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="white" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z"/></svg>
-          george.bluth@reqres.in
+          {email}
         </p>
       </div>
     </div>
